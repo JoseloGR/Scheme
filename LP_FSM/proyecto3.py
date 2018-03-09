49b50e0ef763afd1e35024b179b0ade36b242bae
JoseloGR <jgarcia@nearshoremx.com>
2018-03-08 23:14:05 -0600

    changes

--- LP_FSM/proyecto3.py
+++ LP_FSM/proyecto3.py
@@ -3,6 +3,9 @@
     Jose Luis Garcia Reymundo
     A01063645
     Proyecto 3
+
+    Correr:
+        python proyecto3.py
 """
 import os, sys
 from types import *
@@ -18,19 +21,20 @@ tipos_token = [ 'no_valido', 'Variable', 'Variable', 'Variable', 'no_valido',
 numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 funcion_transicion = {  (0, 'letra'): 1,
                         (0, 'digito'): 6,
+                        (0, 'punto'): 8,
                         (0, 'resta'): 14,
                         (0, 'suma'): 16,
                         (0, 'asignacion'): 17,
                         (0, 'multiplicacion'): 18,
                         (0, 'division'): 19,
                         (0, 'potencia'): 22,
-                        (0, 'simbolo'): 24,
-                        (0, 'simbolo'): 25,
+                        (0, 'simbolo_a'): 24,
+                        (0, 'simbolo_c'): 25,
+                        (0, 'espacio'): 0,
                         (1, 'letra'): 1,
                         (1, 'digito'): 2,
                         (1, 'underscore'): 3,
                         (1, 'fin'): 5,
-                        (1, 'espacio'): 5,
                         (2, 'fin'): 5,
                         (2, 'espacio'): 5,
                         (2, 'digito'): 2,
@@ -38,12 +42,18 @@ funcion_transicion = {  (0, 'letra'): 1,
                         (2, 'letra'): 1,
                         (3, 'digito'): 2,
                         (3, 'letra'): 1,
+                        (3, 'underscore'): 3,
                         (3, 'espacio'): 5,
                         (3, 'fin'): 5,
+                        (5, 'espacio'): 5,
+                        (5, 'fin'): 0,
+                        (5, 'asignacion'): 0,
+                        (5, 'digito'): 0,
                         (6, 'digito'): 6,
                         (6, 'fin'): 7,
                         (6, 'punto'): 8,
                         (8, 'digito'): 8,
+                        (8, 'multiplicacion'): 0,
                         (8, 'fin'): 9,
                         (8, 'E'): 10,
                         (10, 'digito'): 11,
@@ -52,7 +62,11 @@ funcion_transicion = {  (0, 'letra'): 1,
                         (11, 'fin'): 13,
                         (12, 'digito'): 11,
                         (14, 'digito'): 6,
+                        (14, 'letra'): 1,
                         (17, 'espacio'): 0,
+                        (17, 'fin'): 0,
+                        (17, 'digito'): 6,
+                        (18, 'simbolo_a'): 0,
                         (19, 'division'): 21,
                         (21, 'letra'): 23,
                         (21, 'digito'): 23,
@@ -65,9 +79,28 @@ funcion_transicion = {  (0, 'letra'): 1,
                         (21, 'multiplicacion'): 23,
                         (21, 'division'): 23,
                         (21, 'potencia'): 23,
-                        (21, 'simbolo'): 23,
+                        (21, 'simbolo_a'): 23,
+                        (21, 'simbolo_c'): 23,
                         (21, 'fin'): 23,
                         (21, 'espacio'): 23,
+                        (23, 'espacio'): 23,
+                        (23, 'letra'): 23,
+                        (23, 'digito'): 23,
+                        (23, 'underscore'): 23,
+                        (23, 'punto'): 23,
+                        (23, 'E'): 23,
+                        (23, 'resta'): 23,
+                        (23, 'suma'): 23,
+                        (23, 'asignacion'): 23,
+                        (23, 'multiplicacion'): 23,
+                        (23, 'division'): 23,
+                        (23, 'potencia'): 23,
+                        (23, 'simbolo_a'): 23,
+                        (23, 'simbolo_c'): 23,
+                        (23, 'fin'): 23,
+                        (25, 'resta'): 0,
+                        (24, 'resta'): 0,
+                        (25, 'division'): 0
                     }
 estados_finales = [1,2,3,5,6,7,8,9,11,13,14,16,17,18,19,21,22,23,24,25]
 estados_excepciones = [1,2,3,5,6,16,17,18,19,21,22,24]
@@ -84,28 +117,41 @@ def automata(cadena, estado_actual, stack):
             return get_token(estado_actual)
     else:
         entrada = detecta_tipo(cadena[0])
-        if entrada is "fin":
-            return
-        stack.append(cadena[0])
+        if entrada is not "fin":
+            stack.append(cadena[0])
+
+        #print("{} - {} => {}".format(estado_actual, entrada, stack))
 
         if (estado_actual, entrada) in funcion_transicion:
-            if estado_actual in estados_finales and not (estado_actual, detecta_tipo(cadena[1])) in funcion_transicion:
-                print("Token - {} | Tipo => {}".format(stack, get_token(estado_actual)))
-                stack = []
-            return automata(cadena[1:], funcion_transicion[(estado_actual,entrada)], stack)
+            if len(cadena) > 1:
+                if estado_actual in estados_finales and (estado_actual, detecta_tipo(cadena[1])) in funcion_transicion:
+                    return automata(cadena[1:], funcion_transicion[(estado_actual, entrada )], stack)
+                else:
+                    print("Token - {} | Tipo => {}".format(stack, get_token(estado_actual)))
+                    stack = []
+                    return automata(cadena[1:], funcion_transicion[(estado_actual,entrada)], stack)
+            else:
+                if len(stack) is not 0:
+                    print("Token - {} | Tipo => {}".format(stack, get_token(estado_actual)))
+                    stack = []
+                    return automata(cadena[1:], funcion_transicion[(estado_actual,entrada)], stack)
+                return
+
         else:
             if estado_actual in estados_excepciones:
                 print_token(stack, entrada)
-                return automata(cadena[1:], funcion_transicion[(0,entrada)], [])
+                if estado_actual == 1:
+                    estado_actual = 0
+                return automata(cadena[1:], estado_actual, [])
             else:
                 return 'Token no valido'
 
 
 def automata_unitario(cadena, estado_actual):
-    """
-        Automata que detecta el tipo de token de acuerdo a la tabla de transicion
-        Recibe la cadena a evaluar
-        el estado actual
+    """ Automata que detecta el tipo de token de acuerdo a la tabla de transicion
+        Argumentos:
+            name: cadena           summary: Cadena a evaluar        type: list
+            name: estado_actual    summary: Estado actual           type: int
     """
     if len(cadena) is 0:
         if estado_actual in estados_finales:
@@ -136,8 +182,10 @@ def detecta_tipo(elemento):
         return 'potencia'
     elif elemento == "=":
         return 'asignacion'
-    elif elemento == "(" or elemento == ")":
-        return 'simbolo'
+    elif elemento == "(":
+        return 'simbolo_a'
+    elif elemento == ")":
+        return 'simbolo_c'
     elif elemento == "E":
         return 'E'
     elif elemento == "\n":
@@ -170,7 +218,7 @@ def main():
 
     for line in content:
         estado = funcion_transicion[(0,detecta_tipo(line[0]))]
-        automata(line,estado,[] )
+        automata(line, estado, [])
 
 if __name__ == "__main__":
     main()
